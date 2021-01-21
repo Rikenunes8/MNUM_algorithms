7494b49a7aa322f92ea8b58b97298273a3439f18
Henrique Nunes <hrn2001@gmail.com>
2021-01-21 00:28:50 +0000

    Sistem of linear equations

--- Sistemas_de_equacoes_lineares.py
+++ Sistemas_de_equacoes_lineares.py
@@ -1,56 +1,71 @@
-# Khaleski
+# Data
 
-def C1j(A, B, j):
-    return round(A[0][j]/B[0][0], 3)
+A1 = [[7, 2, 6], [4, 10, 1], [5, -2, 8]]
+b1 = [24, 27, 27]
 
-def Bij(A, B, C, i, j):
-    return round(A[i][j] - sum([B[i][k]*C[k][j] for k in range(0, j)]), 3)
-
-def Cij(A, B, C, i, j):
-    return round(1/B[i][i]*(A[i][j]-sum([B[i][k]*C[k][j] for k in range(0, i)])), 3)
+A2 = [[3, 1, 1], [1, 4, 2], [0, 2, 5]]
+b2 = [7, 4, 5]
 
 
-def khaleski():
-    A = [[7, 2, 6], [4, 10, 1], [5, -2, 8]]
+# Algorithms
+
+def cholesky(A, b):
     B = [[0]*3 for _ in range(len(A))]
     C = [[0]*3 for _ in range(len(A))]
-
-    for i in range(len(A)):
+    
+    def C1j(j):
+        return A[0][j]/B[0][0]
+    def Bij(i, j):
+        return A[i][j]-sum(B[i][k]*C[k][j] for k in range(j))
+    def Cij(i, j):
+        return (A[i][j]-sum(B[i][k]*C[k][j] for k in range(i)))/B[i][i]
+    
+    for i in range(3):
         C[i][i] = 1
-
     for i in range(3):
         B[i][0] = A[i][0]
-        C[0][i] = C1j(A, B, i)
+        C[0][i] = C1j(i)
+
     for j in range(1, 3):
         for i in range(j, 3):
-            B[i][j] = Bij(A, B, C, i, j)
+            B[i][j] = Bij(i, j)
         for i in range(j+1, 3):
-            C[j][i] = Cij(A, B, C, j, i)
-    return (B, C)
+            C[j][i] = Cij(j, i)
+
+    Y = [0]*3
+    for i in range(3):
+        Y[i] = (b[i]-B[i][0]*Y[0]-B[i][1]*Y[1])/B[i][i]
+    X = [0]*3
+    for i in range(2, -1, -1):
+        X[i] = (Y[i]-C[i][2]*X[2]-C[i][1]*X[1])/C[i][i]
+    return X
 
 
-# print(khaleski())
-B, C = khaleski()
-
-b = [24, 27, 27]
-
-Y = [0] * 3
-for i in range(3):
-    Y[i] = (b[i]-B[i][0]*Y[0]-B[i][1]*Y[1])/B[i][i]
-# print(Y)
-
-X = [0]*3
-for i in range(2, -1, -1):
-    X[i] = (Y[i]-C[i][2]*X[2]-C[i][1]*X[1])/C[i][i]
-    
-# print(X)
+def jacobi(A, b, x, y, z, e):
+    X = [x, y, z]
+    Xn = [x, y, z]
+    erro = [e+1]*3
+    while any(er > e for er in erro):
+        for i in range(3):
+            Xn[i] = (b[i]-sum(A[i][j]*X[j] for j in range(3) if j != i))/A[i][i]
+        erro = [abs(Xn[i]-X[i]) for i in range(3)]
+        X = Xn.copy()
+    return Xn
 
 
-# Jacobi
+def seidel(A, b, x, y, z, e):
+    X = [x, y, z]
+    Xn = [x, y, z]
+    erro = [e+1]*3
+    while any(er > e for er in erro):
+        for i in range(3):
+            Xn[i] = (b[i]-sum(A[i][j]*Xn[j] for j in range(3) if j != i))/A[i][i]
+        erro = [abs(Xn[i]-X[i]) for i in range(3)]
+        X = Xn.copy()
+    return Xn
 
-# def jacobi(x0, y0, z0, e):
-#     A = [[3, 1, 1], [1, 4, 2], [0, 2, 5]]
-#     b = [7, 4, 5]
+
+# def jacobi(A, b, x0, y0, z0, e):
 #     erro1, erro2, erro3 = e+1, e+1, e+1
 #     while erro1 > e or erro2 > e or erro3 > e:
 #         xn = (b[0] - y0*A[0][1] - z0*A[0][2])/A[0][0]
@@ -62,26 +77,7 @@ for i in range(2, -1, -1):
 #         x0, y0, z0 = xn, yn, zn
 #     return (round(xn), round(yn), round(zn))
 
-def jacobi(X0, e):
-    Xn = [0]*3
-    A = [[3, 1, 1], [1, 4, 2], [0, 2, 5]]
-    b = [7, 4, 5]
-    erro = [e+1]*3
-    while erro[0] > e or erro[1] > e or erro[2] > e:
-        for i in range(3):
-            Xn[i] = (b[i]-sum([A[i][j]*X0[j] for j in range(3) if i != j]))/A[i][i]
-        erro = [abs(Xn[i]-X0[i]) for i in range(3)]
-        X0 = Xn.copy()
-    return Xn
-
-print(jacobi([0, 0, 0], 0.00001))
-
-
-# Seidel
-
-# def seidel(x0, y0, z0, e):
-#     A = [[3, 1, 1], [1, 4, 2], [0, 2, 5]]
-#     b = [7, 4, 5]
+# def seidel(A, b, x0, y0, z0, e):
 #     erro1, erro2, erro3 = e+1, e+1, e+1
 #     while erro1 > e or erro2 > e or erro3 > e:
 #         xn = (b[0] - y0*A[0][1] - z0*A[0][2])/A[0][0]
@@ -93,16 +89,10 @@ print(jacobi([0, 0, 0], 0.00001))
 #         x0, y0, z0 = xn, yn, zn
 #     return (round(xn), round(yn), round(zn))
 
-def seidel(Xn, e):
-    X0 = [1, 1, 1]
-    A = [[3, 1, 1], [1, 4, 2], [0, 2, 5]]
-    b = [7, 4, 5]
-    erro = [e+1]*3
-    while erro[0] > e or erro[1] > e or erro[2] > e:
-        for i in range(3):
-            Xn[i] = (b[i]-sum([A[i][j]*Xn[j] for j in range(3) if i != j]))/A[i][i]
-        erro = [abs(Xn[i]-X0[i]) for i in range(3)]
-        X0 = Xn.copy()
-    return Xn
 
-# print(seidel([0, 0, 0], 0.00001))
+# Run
+
+print(cholesky(A1, b1))
+print(jacobi(A2, b2, 0, 0, 0, 0.00001))
+print(seidel(A2, b2, 0, 0, 0, 0.00001))
+
